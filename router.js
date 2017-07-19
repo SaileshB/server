5831744954fc30fe5027ac91911b1c6e41d96091
MONKEY-D-LUFFY\saile <nhuja08@gmail.com>
2017-07-19 17:59:44 +0545

    added passport-local for login

--- router.js
+++ router.js
@@ -1,7 +1,18 @@
 const Authenitication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+
+const requireAuth = passport.authenticate('jwt', {session: false});
+
+
 module.exports = function (app) {
     // app.get('/', function (req, res, next) {
     //     res.send(['lime', 'vodka', 'sprite']);
     // });
-    app.post('/signup',Authenitication.signup);
+    app.get('/', requireAuth, function (req, res) {
+        res.send({hi: 'there'});
+    });
+
+    app.post('/signup', Authenitication.signup);
 };
--- services/passport.js
+++ services/passport.js
@@ -3,11 +3,41 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+//create local strategy
+const localOptions = {usernameField:'email'};
+const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+    //verify username,password call done with the user
+    //if it is the correct username and password
+    //otherwise cal done with false
+});
+
 
 //setup options for JWT Stragety
-const jwtOptions = {};
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
 
-//create jwt stragety
-const jwtLogin = new JwtStrategy(jwtOptions,function (payload,done) {
+//create jwt strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
+    //see if the userID in the payload exists in our database
+    //if it does ,call 'done' with that order
+    //otherwise , call done without a user object
 
+    User.findById(payload.sub, function (err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
 });
+
+
+//tell passport to use these strategy
+passport.use(jwtLogin);
